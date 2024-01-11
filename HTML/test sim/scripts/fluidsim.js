7cf4693b7c91879f9c45742c07f6718fb8251427
Bobislav <bobo.svilenov@gmail.com>
2024-01-11 14:25:15 +0200

    added boundries to the canvas

--- HTML/test sim/scripts/fluidsim.js
+++ HTML/test sim/scripts/fluidsim.js
@@ -1,7 +1,10 @@
 const canvas = document.getElementById("Workspace");
 const ctx = canvas.getContext("2d");
 const dt = 0.1;
+const width = canvas.width;
+const height = canvas.height;
 let particles = [];
+let bounciness = 0.9;
 
 class Particle{
     constructor(position, velocity, acceleration, radius, color){
@@ -15,13 +18,10 @@ class Particle{
         particles.push(this);
     }
     draw_particle(){
-        ctx.beginPath();
-        ctx.arc(this.position[0], this.position[1], this.radius, 0.2 * Math.PI, false);
         ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.position[0], this.position[1], this.radius, 2 * Math.PI, false);
         ctx.fill();
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = this.color;
-        ctx.stroke();
     }
     update_motion(){
         this.position[0] += this.velocity[0] * dt;
@@ -30,15 +30,36 @@ class Particle{
         this.velocity[1] += this.acceleration[1] * dt;
 
     }
+    bounds(){
+        if(this.position[0] < this.radius){
+            this.position[0] = this.radius;
+            this.velocity[0] = Math.abs(this.velocity[0] * bounciness);
+        }
+        if(this.position[1] < this.radius){
+            this.position[1] = this.radius;
+            this.velocity[1] = Math.abs(this.velocity[1] * bounciness);
+        }
+        if(this.position[0] > width - this.radius){
+            this.position[0] = width - this.radius;
+            this.velocity[0] = - Math.abs(this.velocity[0] * bounciness);
+        }
+        if(this.position[1] > height - this.radius){
+            this.position[1] = height - this.radius;
+            this.velocity[1] = - Math.abs(this.velocity[1] * bounciness);
+        }
+    }
 }
+
+
 
 function Main(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     if(particles.length > 0){
-        for(let point=0; point<particles.length; point++){
+        for(let point = 0; point < particles.length; point++){
             particles[point].draw_particle();
             particles[point].update_motion();
+            particles[point].bounds();
     }}
 
 
@@ -46,8 +67,8 @@ function Main(){
 
 }
 
-new Particle([100, 100], [0, 0], [0, 0], 10, '#FFFFFF')
-new Particle([200, 200], [0, 0], [0, 0], 10, '#FFFFFF')
+new Particle([100, 100], [0, 0], [0, 1], 10, '#FFFFFF');
+new Particle([200, 200], [0, 0], [0, 1], 10, '#FFFFFF');
 
 Main()
 
