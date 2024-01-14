cca57d8335cb2a2ff6911fbc83363a0396cfde67
Bobislav <bobo.svilenov@gmail.com>
2024-01-14 19:59:15 +0200

    round collision detection

--- HTML/Physics Engines/V2/js/Main.js
+++ HTML/Physics Engines/V2/js/Main.js
@@ -4,9 +4,10 @@ function Main(){
     for(let particle = 0; particle < particles.length; particle++){
         particles[particle].drawParticle();
         particles[particle].update();
+        particles[particle].callCollision(particle);
         particles[particle].bounds();
     }
-    if(frameCounter % 100 === 0){
+    if(frameCounter % 10 === 0){
         new ParticleRound(new Vector2D(100, 100), new Vector2D(10,0), new Vector2D(0, 10), 10, 10, "#FFFFFF")
     }
 
--- HTML/Physics Engines/V2/js/engine.js
+++ HTML/Physics Engines/V2/js/engine.js
@@ -40,22 +40,62 @@ class ParticleRound{
 
     bounds(){
         if(this.position.x >= (width/2) - this.radius){
-            this.velocity.x = - Math.abs(this.velocity.x) * bounciness
+            this.velocity.x = - Math.abs(this.velocity.x) * bounciness;
+            this.position.x = width/2 - this.radius;
         }
         if(this.position.x <= this.radius - (width/2)){
-            this.velocity.x = Math.abs(this.velocity.x) * bounciness
+            this.velocity.x = Math.abs(this.velocity.x) * bounciness;
+            this.position.x = - width/2 + this.radius;
         }
         if(this.position.y >= (height/2) - this.radius){
-            this.velocity.y = - Math.abs(this.velocity.y) * bounciness
+            this.velocity.y = - Math.abs(this.velocity.y) * bounciness;
+            this.position.y = height/2 - this.radius;
         }
         if(this.position.y <= this.radius - (height/2)){
-            this.velocity.y = Math.abs(this.velocity.y) * bounciness
+            this.velocity.y = Math.abs(this.velocity.y) * bounciness;
+            this.position.y = - height/2 + this.radius;
         }
 
         return;
     }
 
+    collideRound(p2, d){
+        let overlap = this.radius + p2.radius - d;
+        let collisionNorm = this.position.sub(p2.position).norm();
+        let velocitySFactor = this.velocity.sub(p2.velocity).dot(collisionNorm);
+        let totMass = this.mass + p2.mass;
+
+
+        this.position = this.position.add(collisionNorm.scale(overlap));
+        p2.position = p2.position.sub(collisionNorm.scale(overlap));
+        
+        this.velocity = this.velocity.sub(collisionNorm.scale(velocitySFactor * 2 * p2.mass / totMass));
+        p2.velocity = p2.velocity.add(collisionNorm.scale(velocitySFactor * 2 * this.mass / totMass));
+
+        return;
+    }
+
+    callCollision(p1idx){
+        for(let p2idx = p1idx; p2idx < particles.length; p2idx++){
+            let p2 = particles[p2idx];
+            if(this != p2){
+                if(p2.shape == "round"){
+                    let d = this.distanceTo(p2);
+                    if(d <= this.radius + p2.radius){
+                        this.collideRound(p2, d);
+                    }
+                }
+            }
+        }
+    }
+
+    distanceTo(p2){
+        let dx = this.position.x - p2.position.x;
+        let dy = this.position.y - p2.position.y;
+        let d = Math.sqrt((dx**2) + (dy**2));
     
+        return d;
+    }
 
     drawParticle(){
         let position = calculateScreenPosition(this.position);
@@ -101,19 +141,31 @@ class ParticleRect{
 
     bounds(){
         if(this.position.x >= width/2 - this.size){
-            this.velocity.x = - Math.abs(this.velocity.x) * bounciness
+            this.velocity.x = - Math.abs(this.velocity.x) * bounciness;
+            this.position.x = width/2 - this.size;
         }
         if(this.position.x <= - width/2){
-            this.velocity.x = Math.abs(this.velocity.x) * bounciness
+            this.velocity.x = Math.abs(this.velocity.x) * bounciness;
+            this.position.x = - width/2 + this.size;
         }
         if(this.position.y >= height/2 - this.size){
-            this.velocity.y = - Math.abs(this.velocity.y) * bounciness
+            this.velocity.y = - Math.abs(this.velocity.y) * bounciness;
+            this.position.y = height/2 - this.size;
         }
         if(this.position.y <= - height/2){
-            this.velocity.y = Math.abs(this.velocity.y) * bounciness
+            this.velocity.y = Math.abs(this.velocity.y) * bounciness;
+            this.position.y = - height/2 + this.size;
         }
 
         return;
+    }
+
+    distanceTo(p2){
+        let dx = this.position.x - p2.position.x;
+        let dy = this.position.y - p2.position.y;
+        let d = Math.sqrt((dx**2) + (dy**2));
+    
+        return d;
     }
 
     drawParticle(){
