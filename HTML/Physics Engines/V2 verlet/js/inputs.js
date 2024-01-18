7b9e719e67237783b5b46cb1f053eff8404efb78
Bobislav <bobo.svilenov@gmail.com>
2024-01-18 17:15:47 +0200

    not so final after all

--- HTML/Physics Engines/V2 verlet/js/inputs.js
+++ HTML/Physics Engines/V2 verlet/js/inputs.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("simArea");
 let camV = new Vector2D(0,0);
 let mousePos;
+let mousePosLast = new Vector2D(0, 0);
 let centeredOnIndx = -1;
 let spawnParticles = false;
 
@@ -60,9 +61,15 @@ canvas.addEventListener("mousemove", (pos) => {
     mousePos = new Vector2D(pos.clientX, pos.clientY);
 });
 
-canvas.addEventListener("click", (pressed) => {
-    if(pressed.button == 0){
-        new ParticleRound(calculatePointPosition(mousePos), calculatePointPosition(mousePos), new Vector2D(0, 0), 10, 10, "#FFFFFF", "down");
-    }
+canvas.addEventListener("mousedown", (pressed) => {
+    mousePosLast = mousePos;
+});
+
+canvas.addEventListener("mouseup", (pressed) => {
+    let mouseNorm = mousePos.sub(mousePosLast).scale(1/50);
+    mousePosLast = mousePos.sub(mouseNorm);
+
+    new ParticleRound(calculatePointPosition(mousePos), calculatePointPosition(mousePosLast), new Vector2D(0, 0), 10, 10, "#FFFFFF", "OTO");
+
 });
 
