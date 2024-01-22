b319878cd29424cc5d17ca5002e4869730e88644
Bobislav <bobo.svilenov@gmail.com>
2024-01-22 12:33:44 +0200

    snek

--- HTML/online game/Snake Online/app/js/snek.js
+++ HTML/online game/Snake Online/app/js/snek.js
@@ -1,5 +1,29 @@
 class snek {
-    constructor(pos, dir, color) {
-        
+    constructor (pos, dir, color, thickness) {
+        this.pos = pos;
+        this.dir = dir;
+        this.color = color;
+        this.thickness = thickness;
+        this.up = new Vector2D(0, -thickness);
+        this.down = new Vector2D(0, thickness);
+        this.right = new Vector2D(thickness, 0);
+        this.left = new Vector2D(-thickness, 0);
+
+        this.snekLength = [pos];
     }
+
+    update () {
+        for (let c = this.snekLength.length; c = 0; c--) {
+
+        };
+        if (this.dir == 1) {
+            this.pos = this.pos.add(this.up);
+        } else if (this.dit == 2) {
+            this.pos = this.pos.add(this.right);
+        } else if (this.dir == 3) {
+            this.pos = this.pos.add(this.down);
+        } else if (this.dir == 4) {
+            this.pos = this.pos.add(this.left);
+        };
+    };
 };
